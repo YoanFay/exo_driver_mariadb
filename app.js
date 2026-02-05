c90d4d3690de3f776330e438afc36b9c7836b49b
YoanFay <yoanfayolle.yf@gmail.com>
2026-02-05 16:20:29 +0100

    build(etudiant): function for add etudiant

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ let connection;
 
 // ETUDIANT
 
+/**
+ * 
+ * @returns
+ */
 async function getAllEtudiant() {
     try {
         connection = await connectionPool.getConnection();
@@ -23,8 +27,49 @@ async function getAllEtudiant() {
         return rows;
     }
     catch (error) {
-        console.log(error)
+        console.log(error);
+        connectionPool.end();
+    }
+}
+
+async function addEtudiant(
+    etudiant_num_carte_identite,
+    etudiant_nom,
+    etudiant_prenom,
+    etudiant_email = null,
+    etudiant_annees_experience = null,
+    etudiant_telephone = null,
+    etudiant_adresse_ligne_1 = null,
+    etudiant_adresse_ligne_2 = null,
+    etudiant_code_postal = null,
+    etudiant_ville = null,
+    etudiant_niveau_etudes = null
+) {
+    try {
+        connection = await connectionPool.getConnection();
+        const rows = await connection.query(
+            `INSERT INTO etudiant(
+            etudiant_num_carte_identite, 
+            etudiant_nom,
+            etudiant_prenom,
+            tuteur_id
+        )
+            VALUES (
+            '${etudiant_num_carte_identite}',
+            '${etudiant_nom}',
+            '${etudiant_prenom}',
+            1
+            )`
+        );
+        connectionPool.end();
+        return rows;
+    }
+    catch (error) {
+        console.log(error);
+        connectionPool.end();
     }
 }
 
 // COURS
+
+console.log(await addEtudiant('CI-0009', 'Daniel', 'Antoine'))
